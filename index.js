eb8c356da1502b6ca286472e00e64f8903929dca
ahmetalpergit <ahmetalperwork@gmail.com>
2023-05-12 01:48:01 +0200

    initial mobile responsive styling

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const shareBtn = document.querySelector('.shareBtn');
 const formContainer = document.querySelector('form');
 
 shareBtn.addEventListener('click', () => {
+  shareBtn.classList.toggle('hidden');
+
   if (formContainer.classList.contains('visible')) {
     formContainer.classList.remove('visible');
-    shareBtn.textContent = 'Share a fact';
   } else {
     formContainer.classList.add('visible');
-    shareBtn.textContent = 'Cancel';
   }
 });
